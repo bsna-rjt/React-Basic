5574ed930d4852818b955e13a8b81f5e0ddf8661
bsna-rjt <basnaranjitkar1138@gmail.com>
2023-10-16 16:59:29 +0545

    Old method in react , deprecated in new versions

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,41 @@
-import React from 'react'
-import Button from './Button';
+// import React from 'react'
+// import Button from './Button';
+import {Component} from 'react';
 
-const Home = () => {
-  return (
-    <div>
-    <h1>Home Page</h1>
-    <h1>Hello</h1>
-    <Button textInsideButton="Home"/>
+// const Home = () => {
+//   return (
+//     <div>
+//     <h1>Home Page</h1>
+//     <h1>Hello</h1>
+//     <Button textInsideButton="Home"/>
     
 
-    </div>
-  )
+//     </div>
+//   )
+// }
+
+
+class Home extends Component { //Old method in React | deprecated already in new versions
+  constructor(){
+    super()
+
+    this.state={
+      count:0
+    }
+  }
+
+  increaseCounter(){
+    this.setState({count:this.state.count+1})
+  }
+
+  render(){
+    return(
+      <div>
+       <h1>Count:{this.state.count}</h1>
+       <button onClick={()=>this.increaseCounter()}>+</button>
+      </div>
+    )
+  }
 }
 
 export default Home
