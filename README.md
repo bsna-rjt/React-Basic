2c573434a064b53731faa3e657848ca5ca5e34f5
bsna-rjt <basnaranjitkar1138@gmail.com>
2023-10-16 17:15:50 +0545

    [HOOKS]: function Vs UseState for re-render power

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-rafce shortcut to create component in .jsx file.
+Extensions:
+ES7+
+Console Ninja
+
+rafce shortcut to create component in .jsx file. 
+
+npm create vite@latest
+
+npm run dev
+
+npm install react-router-dom
 
 npm install vite -g
 
 npm i prop-types
+
+Hooks:
+useState for re-render
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,41 +1,58 @@
 // import React from 'react'
+import { useState } from 'react';
 // import Button from './Button';
-import {Component} from 'react';
+// import {Component} from 'react';
 
-// const Home = () => {
-//   return (
-//     <div>
-//     <h1>Home Page</h1>
-//     <h1>Hello</h1>
-//     <Button textInsideButton="Home"/>
-    
+const Home = () => {
+  //useState for re-render
+  const state=useState(0); //[0,function]
+  // console.log("state[0]",state[0])
+  // console.log("state[1]",state[1])
+  const state1=state[0]; //[initial value
+  const state2=state[1]; //function
+  const [count,setCount]=useState(0);
+  
+  // var count=0;
+  // const increaseCount=()=>{ //function use garda re-render hudaina //so useState use gareko for re-render feature
+  //   count=count+1;
+  // }
 
-//     </div>
-//   )
-// }
-
-
-class Home extends Component { //Old method in React | deprecated already in new versions
-  constructor(){
-    super()
-
-    this.state={
-      count:0
-    }
-  }
-
-  increaseCounter(){
-    this.setState({count:this.state.count+1})
-  }
-
-  render(){
-    return(
-      <div>
-       <h1>Count:{this.state.count}</h1>
-       <button onClick={()=>this.increaseCounter()}>+</button>
-      </div>
-    )
-  }
+  return (
+    <div>
+    <h1>Home Page</h1>
+    <h1>state1:{state1}</h1>
+    <h1>count:{count}</h1>
+    <button onClick={()=>state2(5)}>+</button>
+    <button onClick={()=>state2(state1+1)}>++</button>
+    <button onClick={()=>setCount(count+1)}>+++</button>
+    {/* <button onClick={increaseCount}>increaseCount</button>  */}
+    {/* <Button textInsideButton="Home"/> */}
+    </div>
+  )
 }
+
+
+// class Home extends Component { //Old method in React | deprecated already in new versions
+//   constructor(){
+//     super()
+
+//     this.state={
+//       count:0
+//     }
+//   }
+
+//   increaseCounter(){
+//     this.setState({count:this.state.count+1})
+//   }
+
+//   render(){
+//     return(
+//       <div>
+//        <h1>Count:{this.state.count}</h1>
+//        <button onClick={()=>this.increaseCounter()}>+</button>
+//       </div>
+//     )
+//   }
+// }
 
 export default Home
